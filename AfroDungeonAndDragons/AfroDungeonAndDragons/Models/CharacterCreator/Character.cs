fcfcdb24733ff0dd710b77517343e0572858fbf8
Afester96 <54309941+Afester96@users.noreply.github.com>
2021-07-29 12:44:00 +0300

    feat: github connected

---
+++ AfroDungeonAndDragons/AfroDungeonAndDragons/Models/CharacterCreator/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfroDungeonAndDragons.Models.CharacterCreator
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string CharacterName { get; set; }
+        public string Class { get; set; }
+        public int Level { get; set; }
+        public string Backgrount { get; set; }
+        public string PlayerName { get; set; }
+        public string Race { get; set; }
+        public string Alignment { get; set; }
+        public int EperiencePoints { get; set; }
+    }
+}
---
+++ AfroDungeonAndDragons/AfroDungeonAndDragons/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfroDungeonAndDragons.Models.CharacterCreator
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public List<Character> Characters { get; set; }
+        public User()
+        {
+            Characters = new List<Character>();
+        }
+    }
+}
