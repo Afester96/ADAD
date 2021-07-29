b2ff496334285bb339e2de0d422b1066e1a6f419
Afester96 <54309941+Afester96@users.noreply.github.com>
2021-07-29 14:03:37 +0300

    feat: added models

--- AfroDungeonAndDragons/AfroDungeonAndDragons/Models/CharacterCreator/Character.cs
+++ AfroDungeonAndDragons/AfroDungeonAndDragons/Models/CharacterCreator/Character.cs
@@ -11,10 +11,16 @@ namespace AfroDungeonAndDragons.Models.CharacterCreator
         public string CharacterName { get; set; }
         public string Class { get; set; }
         public int Level { get; set; }
-        public string Backgrount { get; set; }
+        public string Background { get; set; }
         public string PlayerName { get; set; }
-        public string Race { get; set; }
+        public Race Race { get; set; }
         public string Alignment { get; set; }
         public int EperiencePoints { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity {get;set;}
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
     }
 }
