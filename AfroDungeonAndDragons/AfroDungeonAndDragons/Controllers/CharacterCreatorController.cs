b14b551ecf51c30731cdc7462ef0bd953ee966be
Afester96 <54309941+Afester96@users.noreply.github.com>
2021-07-30 17:20:33 +0300

    feat: added race controller and view

---
+++ AfroDungeonAndDragons/AfroDungeonAndDragons/Controllers/CharacterCreatorController.cs
@@ -0,0 +1,28 @@
+﻿using AfroDungeonAndDragons.Models.CharacterCreator;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfroDungeonAndDragons.Controllers
+{
+    public class CharacterCreatorController : Controller
+    {
+        List<Race> races;
+        public CharacterCreatorController()
+        {
+            races = new List<Race>
+            {
+                new Race {Id=1, Name="Дварф", Size=Size.Tiny, Speed=30, ShortDescription="Полные древнего величия королевства и вырезанные в " +
+                "толще гор чертоги, удары кирок и молотков,раздающиеся в глубоких шахтах и пылающий кузнечный горн, верность клану и традициям и пылающая " +
+                "ненависть к гоблинам и оркам — вот вещи, объединяющие всех дварфов.", Description="Описание", RacialTrait="Short and Stout"},
+                new Race {Id=2, Name="Человек", Size=Size.Medium, Speed=30, ShortDescription="Человеки", Description="Описание", RacialTrait="Short and Stout"}
+            };
+        }
+        public IActionResult Races()
+        {
+            return View(races);
+        }
+    }
+}
--- AfroDungeonAndDragons/AfroDungeonAndDragons/Models/CharacterCreator/Race.cs
+++ AfroDungeonAndDragons/AfroDungeonAndDragons/Models/CharacterCreator/Race.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,14 @@ namespace AfroDungeonAndDragons.Models.CharacterCreator
     public class Race
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public Size Size { get; set; }
         public int Speed { get; set; }
+        [Required]
         public string ShortDescription { get; set; }
         public string Description { get; set; }
         public string RacialTrait { get; set; }
+
     }
 }
